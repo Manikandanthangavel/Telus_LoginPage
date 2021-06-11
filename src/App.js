5027a9dfcf0e3f2034238862fc6f1b07900b065f
mani <exit>
2021-06-11 17:18:07 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import Login from './Login'
 import './App.css';
-
+import './myStyles.css'
+import log from './pictures/logo.jpg'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+    return (
+      <div>
+         <html>
+           <header>
+          <div className='container'>
+            <div className='navbar-brand navbar-brand'>
+            <a href='http://www.telus.com'>
+             <img  src={log} width='230px'></img> 
+          </a>
+            </div>
+          </div>
+          </header>
+          <br></br>
+          <Login/>
+          <footer>
+
+
+    <div className="footer">
+        <p><i className="fa fa-lock color-green"></i>This is a secure page</p>
+        <div className="divider"></div>
+        <img src={log} width='154px' alt='90px'></img>
+        <p>© 2021 TELUS Communications Inc.</p>
+
+
+</div>
+
+</footer>
+          </html>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import './myStyles.css'
+
+class Login extends Component {
+    render() {
+        return(
+<div>
+<section className="loginbox">
+        <div className="container">
+            <h1 className='kk'>MY TELUS</h1>
+        </div>
+       
+        <div className="container">
+            <div className='center-box'>
+            <h2>Log in</h2>
+            <p>Enter your existing TELUS account details here</p>
+            <form className='form'>
+                <div className='as'>
+                <p> Email / Username</p>
+                <input type='text' className='username'>
+                </input>
+                </div>
+                <div className='as'>
+                 <p>Password &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                 <a href="https://www.telus.com" className='color-purple'>Forgot?
+               </a>
+               </p>
+               <div className="form-input">
+				<span className='icon'><a href='#'><button className="btn"><i class="fa"></i>show</button></a></span>   
+                <input type="password" className="password"></input>        
+                </div> 
+            </div>
+            <br></br>
+            <div className="checkbox">
+			   <input id="remember-me-checkbox" type="checkbox"  tabindex="4"></input>
+               <label for="remember-me-checkbox">Remember me </label>
+                </div>
+			<br></br>
+            <button className="btn btn-success" type="submit" value="Next">Next</button>
+            <div>
+                <br></br>
+            <p>Having trouble logging in?
+               <a href="https://www.telus.com"
+                className='yes'>Click here to log in with other account details</a>
+           </p> </div>
+           <br></br>
+           <p className="ar">New to TELUS? <br></br><br></br>
+                   <button className="btn btn-success">Register now > </button>
+             </p>
+            </form>
+        </div>
+       </div>
+    </section>
+    <div className="hp">
+           <a href="#" className="nh">Need help ? Let’s Chat! <i className="fa fa-angle-right" aria-hidden="true"></i></a>
+            </div>
+</div>
+        );
+    }
+}
+
+export default Login;
